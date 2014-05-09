336ef25e7e69424b6d2ee1cf240591f3109b64be
Clicky <msullivan@blackpearlsystems.com>
2014-05-09 14:40:55 -0700

    Make influxdb more flexible
    Allow users to provide a function to influxdb to pull other data out of
    events and pass them on to influxdb.

--- src/riemann/influxdb.clj
+++ src/riemann/influxdb.clj
@@ -11,7 +11,10 @@
      (join "." split-service)))
 
 (defn influxdb
-  [opts]
+    "Construct a client to InfluxDB and forward events to it.
+     Takes an optional parameter fields which returns additional fields
+     other name, host, state and value to send to influx given an event."
+  [opts & [fields]]
   (let [opts (merge {:host "127.0.0.1"
                      :port 8086
                      :scheme "http"
@@ -19,12 +22,14 @@
                      :password "root"
                      :db "riemann"
                      :series "riemann-events" } opts)
+        fields (if (not= fields nil) fields (fn [e] {}))
         client (influx/make-client opts)]
     (fn [event]
     (when (:metric event)
       (when (:service event)
         (when (:host event)
-          (influx/post-points client (:series opts) [{:name (influxdb-metric-name event)
-                                                      :host (:host event)
-                                                      :state (:state event)
-                                                      :value (:metric event)}])))))))
+          (influx/post-points client (:series opts)
+          [(merge (fields event) {:name (influxdb-metric-name event)
+                                  :host (:host event)
+                                  :state (:state event)
+                                  :value (:metric event)})])))))))
